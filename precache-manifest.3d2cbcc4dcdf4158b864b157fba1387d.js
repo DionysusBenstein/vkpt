cac84e89a9496162316c97b354408a1da59c378e
DionysusBenstein <dionysusbenstein@gmail.com>
2021-07-07 19:45:49 +0300

    Add files

---
+++ precache-manifest.3d2cbcc4dcdf4158b864b157fba1387d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "81f8a81a7e2205c44ebf",
+    "url": "/vkpt/static/css/main.60449815.chunk.css"
+  },
+  {
+    "revision": "81f8a81a7e2205c44ebf",
+    "url": "/vkpt/static/js/main.0bf14fac.chunk.js"
+  },
+  {
+    "revision": "869c24b9d46b1d9a0ef4",
+    "url": "/vkpt/static/js/runtime~main.bbbf44d6.js"
+  },
+  {
+    "revision": "38fc9fec0eea31eb872a",
+    "url": "/vkpt/static/js/2.88c4d4c5.chunk.js"
+  },
+  {
+    "revision": "eaef9d5703d302df59a052fd5b218c5b",
+    "url": "/vkpt/static/media/icon.eaef9d57.svg"
+  },
+  {
+    "revision": "6732bd426c60b47172b1c7296e886a60",
+    "url": "/vkpt/static/media/no-img.6732bd42.png"
+  },
+  {
+    "revision": "a5c7ffe662ee034239112a47b023db57",
+    "url": "/vkpt/index.html"
+  }
+];
